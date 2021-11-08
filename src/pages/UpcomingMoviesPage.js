a82f0dea8aaafb9f04d25f70516525ffe5ce8938
Ruoye Qian <20095880@mail.wit.ie>
2021-11-08 23:39:51 +0000

    Change upcoming movies page to use caching.

--- src/pages/UpcomingMoviesPage.js
+++ src/pages/UpcomingMoviesPage.js
@@ -3,7 +3,6 @@ import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 import PlayListAdd from "../components/cardIcons/playListAdd";
 
 const UpcomingMoviesPage = (props) => {
