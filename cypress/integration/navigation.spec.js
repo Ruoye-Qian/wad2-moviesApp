2590f102e88d421606b45cae1048f87420551297
Ruoye Qian <20095880@mail.wit.ie>
2021-11-16 12:52:12 +0000

    CI - Add test stage to the pipeline.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -64,7 +64,7 @@ describe("Navigation", () => {
     beforeEach(() => {
       cy.get("button[aria-label='add to favorites']").eq(0).click();
       cy.get("button[aria-label='add to favorites']").eq(1).click();
-      cy.get("header").find(".MuiToolbar-root").find("button").eq(1).click();
+      cy.get("header").find(".MuiToolbar-root").find("button").eq(2).click();
     });
     it("should navigate to the movies detail page and change the browser URL", () => {
       cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
