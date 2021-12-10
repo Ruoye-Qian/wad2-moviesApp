debb484883ab42ef53d3bfa245db0d98789e3aae
Ruoye Qian <20095880@mail.wit.ie>
2021-12-10 03:20:04 +0000

    implement headerPerson and related stories

---
+++ src/stories/personHeader.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PersonHeader from "../components/headerPerson";
+import SamplePerson from "./samplePersonData";
+import { MemoryRouter } from "react-router";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "Person Details Page/PersonHeader",
+  component: PersonHeader,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+  ],
+};
+
+export const Basic = () => <PersonHeader person={SamplePerson} />;
+
+Basic.storyName = "Default";
