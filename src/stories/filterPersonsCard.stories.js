38211f3ac39a8de7ffd3c83fa6f6deb10c6a1999
Ruoye Qian <20095880@mail.wit.ie>
2021-12-10 04:59:08 +0000

    implement filterPerson and related stories

---
+++ src/stories/filterPersonsCard.stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FilterPersonsCard from "../components/filterPersonsCard";
+import { MemoryRouter } from "react-router";
+import { action } from "@storybook/addon-actions";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default {
+  title: "Home Page/FilterPersonsCard",
+  component: FilterPersonsCard,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => (
+      <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
+    ),
+  ],
+};
+
+export const Basic = () => {
+  return <FilterPersonsCard onUserInput={action("filter input")} />;
+};
+Basic.storyName = "Default";
