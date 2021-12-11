150d97c1221ebfd7b96d68b86d5f4b6f950ff549
Ruoye Qian <20095880@mail.wit.ie>
2021-12-11 22:38:09 +0000

    E2E test -favourite page

---
+++ cypress/integration/favourite-page.spec.js
@@ -0,0 +1,154 @@
+let movies;    // List of movies from TMDB
+
+// Utility functions
+const filterByTitle = (movieList, string) =>
+    movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+    movieList.filter((m) => m.genre_ids.includes(genreId));
+
+const filterByTitleAndGenre =(moveList,string,genreId)=>
+    moveList.filter((m)=> m.title.toLowerCase().search(string) !== -1 && m.genre_ids.includes(genreId));
+  
+
+describe("Favourite Page", () => {
+    before(() => {
+        // Get movies from TMDB and store in movies variable.
+        cy.request(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+                "TMDB_KEY"
+            )}&language=en-US&include_adult=false&include_video=false&page=1`
+        )
+            .its("body")    // Take the body of HTTP response from TMDB
+            .then((response) => {
+                movies = response.results
+            })
+    })
+    beforeEach(() => {
+        cy.visit("/")
+        // cy.wait(1000)
+    });
+
+    describe("Base test", () => {
+        it("displays page header", () => {
+            cy.get("Button").eq(2).click();
+            cy.get("h3").contains("Favourite Movies");
+        });
+    })
+
+    describe("Filtering", () => {
+        describe("By movie title", () => {
+         it("should only display movies with m in the title", () => {
+           let searchString = "m";
+           let matchingMovies = filterByTitle(movies, searchString);
+           cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+           cy.get(".MuiCardHeader-content").should(
+             "have.length",
+             matchingMovies.length
+           );
+           cy.get(".MuiCardHeader-content").each(($card, index) => {
+             cy.wrap($card).find("p").contains(matchingMovies[index].title);
+           });
+         })
+         it("should only display movies with o in the title", () => {
+           let searchString = "o";
+           let matchingMovies = filterByTitle(movies, searchString);
+           cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+           cy.get(".MuiCardHeader-content").should(
+             "have.length",
+             matchingMovies.length
+           );
+           cy.get(".MuiCardHeader-content").each(($card, index) => {
+             cy.wrap($card).find("p").contains(matchingMovies[index].title);
+           });
+         });
+         it("should display the exceptional case with no matches", () => {
+            let searchString = "xyz";
+            let matchingMovies = filterByTitle(movies, searchString);
+            cy.get("#filled-search").clear().type(searchString); 
+            cy.get(".MuiCardHeader-content").should(
+              "have.length",
+              matchingMovies.length
+            );
+          });
+       });
+        describe("By movie genre", () => {
+         it("should display movies with the specified genre only", () => {
+           const selectedGenreId = 35;
+           const selectedGenreText = "Comedy";
+           const matchingMovies = filterByGenre(movies, selectedGenreId);
+           cy.get("#genre-select").click();
+           cy.get("li").contains(selectedGenreText).click();
+           cy.get(".MuiCardHeader-content").should(
+             "have.length",
+             matchingMovies.length
+           );
+           cy.get(".MuiCardHeader-content").each(($card, index) => {
+             cy.wrap($card).find("p").contains(matchingMovies[index].title);
+           });
+         });
+        });
+
+        describe("By movie title and genre", () => {
+            it("should display movies with o in the title and matching the specified genre", () => {
+              const selectedGenreId = 35;
+              const selectedGenreText = "Comedy";
+              //const genrematchingMovies=filterByGenre(movies,selectedGenreId);
+              let searchString = "o";
+              //const matchingMovies=filterByTitle(genrematchingMovies,searchString);
+              const matchingMovies=filterByTitleAndGenre(movies,searchString,selectedGenreId);
+              cy.get("#filled-search").clear().type(searchString); 
+              cy.get("#genre-select").click();
+              cy.get("li").contains(selectedGenreText).click();
+              cy.get(".MuiCardHeader-content").should(
+                "have.length",
+                matchingMovies.length
+              );
+              cy.get(".MuiCardHeader-content").each(($card, index) => {
+                cy.wrap($card).find("p").contains(matchingMovies[index].title);
+              });
+            });
+            it("should display the exceptional case with no matches", () => {
+                let searchString = "xyz";
+                const selectedGenreId = 35;
+                const selectedGenreText = "Comedy";
+                let matchingMovies = filterByTitleAndGenre(movies,searchString,selectedGenreId);
+                cy.get("#filled-search").clear().type(searchString); 
+                cy.get("#genre-select").click();
+                cy.get("li").contains(selectedGenreText).click();
+                cy.get(".MuiCardHeader-content").should(
+                  "have.length",
+                  matchingMovies.length
+                );
+            });
+        });
+    });
+
+    describe("delete favourite movie", () => {
+        it("should delete the selected movies on the Favourites page", () => {  
+          cy.get("button[aria-label='add to favorites']").eq(0).click();
+          cy.get("button[aria-label='add to favorites']").eq(2).click();
+          cy.get(".MuiAvatar-root").should("have.length",2);  
+          // Check the Favourites page.
+          cy.get("header").find(".MuiToolbar-root").find("button").eq(2).click();
+          cy.get(".MuiCardHeader-content").should("have.length",2);
+          cy.get(".MuiCardHeader-content").eq(0).find("p").contains(movies[0].title)
+          cy.get(".MuiCardHeader-content").eq(1).find("p").contains(movies[2].title)
+          cy.get("button[aria-label='remove from favorites']").eq(0).click();
+          cy.get("button[aria-label='remove from favorites']").eq(0).click();
+          cy.get(".MuiAvatar-root").should("have.length",0); 
+        });
+      });
+
+    describe("Add to write-review button test", () => {
+        it("should get to the write review page", () => {
+            cy.get("button[aria-label='add to favorites']").eq(0).click();
+            cy.get("header").find(".MuiToolbar-root").find("button").eq(2).click();
+            cy.get(".MuiCardHeader-content").eq(0).find("p").contains(movies[0].title)
+            cy.get("button[aria-label='write reviews']").eq(0).click();
+            cy.contains("Write a review").click();
+            cy.url().should("include", `/reviews/form`);
+        });
+    })
+
+})
--- src/components/cardIcons/writeReview.js
+++ src/components/cardIcons/writeReview.js
@@ -1,9 +1,11 @@
 import React from "react";
 import RateReviewIcon from "@material-ui/icons/RateReview";
 import { Link } from "react-router-dom";
+import IconButton from "@material-ui/core/IconButton";
 
 const WriteReviewIcon = ({ movie }) => {
   return (
+    <IconButton aria-label="write reviews" >
     <Link
       to={{
         pathname: `/reviews/form`,
@@ -14,6 +16,7 @@ const WriteReviewIcon = ({ movie }) => {
     >
       <RateReviewIcon color="primary" fontSize="large" />
     </Link>
+    </IconButton>
   );
 };
 
