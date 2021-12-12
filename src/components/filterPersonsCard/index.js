db9c001901e6c4a0d79eed46b8cb7ef5fc35bb4b
Ruoye Qian <20095880@mail.wit.ie>
2021-12-12 00:18:36 +0000

    preparation before CI test

--- src/components/filterPersonsCard/index.js
+++ src/components/filterPersonsCard/index.js
@@ -18,7 +18,7 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
+//import FormLabel from '@material-ui/core/FormLabel';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -37,7 +37,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FilterPersonsCard(props) {
   const classes = useStyles();
-  const [value, setValue] = React.useState('ASC');
+  // const [value, setValue] = React.useState('ASC');
+  const [value] = React.useState('ASC');
 
   const handleChange = (e, type, value) => {
     e.preventDefault();
