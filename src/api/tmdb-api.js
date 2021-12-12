986f70ab2382eb9eb4b48bc413852c5f60c5aacc
Ruoye Qian <20095880@mail.wit.ie>
2021-12-12 17:45:52 +0000

    preparation for second E2E test

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -254,16 +254,16 @@ export const getTv = (args) => {
 };
 
 
-export const getTvRecommendation = (id) => {
-  return fetch(
-    `https://api.themoviedb.org/3/tv/${id}/recommendations?api_key=${process.env.REACT_APP_TMDB_KEY}`
-  )
-    .then((res) => res.json())
-    .then((json) => {
-      // console.log(json.results);
-      return json.results;
-    });
-};
+// export const getTvRecommendation = (id) => {
+//   return fetch(
+//     `https://api.themoviedb.org/3/tv/${id}/recommendations?api_key=${process.env.REACT_APP_TMDB_KEY}`
+//   )
+//     .then((res) => res.json())
+//     .then((json) => {
+//       // console.log(json.results);
+//       return json.results;
+//     });
+// };
 
 export const getTvCast = id => {
   return fetch(
